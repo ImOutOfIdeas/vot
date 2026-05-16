b2adea6d1270c703d05447dd743cc8879381fdf6
Jonathan Doyle <64992347+ImOutOfIdeas@users.noreply.github.com>
2026-05-16 00:18:41 -0400

    Add files via upload

--- client.go
+++ client.go
@@ -16,6 +16,7 @@ func main() {
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 
 	// Connect to UDP server
+	//addr, err := net.ResolveUDPAddr("udp", "75.188.38.21:9000")
 	addr, err := net.ResolveUDPAddr("udp", "192.168.1.122:9000")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error resolving UDP address: ", err)
@@ -43,13 +44,15 @@ func main() {
 	defer portaudio.Terminate()
 
 	// Create input stream with capture callback closure
-	istream, err := portaudio.OpenDefaultStream(1, 0, 44100, 1024,
+	istream, err := portaudio.OpenDefaultStream(1, 0, 48000, 1024,
 		// Arrange samples into little endian byte array and send to server
-		func(in []int16) {
-			bytes := make([]byte, len(in)*2)
+		func(in []int32) {
+			bytes := make([]byte, len(in)*4)
 			for i, sample := range in {
-				bytes[i*2] = byte(sample)
-				bytes[i*2+1] = byte(sample >> 8)
+				bytes[i*4] = byte(sample)
+				bytes[i*4+1] = byte(sample >> 8)
+				bytes[i*4+2] = byte(sample >> 16)
+				bytes[i*4+3] = byte(sample >> 24)
 			}
 			conn.Write(bytes)
 		})
@@ -61,14 +64,14 @@ func main() {
 	defer istream.Stop()
 
 	// Create channel for samples received from the server
-	output_channel := make(chan []int16, 10)
+	output_channel := make(chan []int32, 10)
 
 	// Create output stream with playback callback closure
-	ostream, err := portaudio.OpenDefaultStream(0, 1, 44100, 1024,
+	ostream, err := portaudio.OpenDefaultStream(0, 1, 48000, 1024,
 		// Fills output buffer with data from output_channel
-		func(out []int16) {
+		func(out []int32) {
 			select {
-			case samples := <- output_channel:
+			case samples := <-output_channel:
 				copy(out, samples)
 			default:
 				for i := range out {
@@ -85,7 +88,7 @@ func main() {
 
 	// Receive UDP packets and push samples into the output_channel
 	go func() {
-		bytes := make([]byte, 1024*2)
+		bytes := make([]byte, 1024*4)
 		for {
 			cnt, err := conn.Read(bytes)
 			if err != nil {
@@ -93,9 +96,12 @@ func main() {
 				sig <- syscall.SIGTERM
 				return
 			}
-			samples := make([]int16, cnt/2)
+			samples := make([]int32, cnt/4)
 			for i := range samples {
-				samples[i] = int16(bytes[i*2]) | int16(bytes[i*2+1]) << 8
+				samples[i] = int32(bytes[i*4]) |
+					int32(bytes[i*4+1])<<8 |
+					int32(bytes[i*4+2])<<16 |
+					int32(bytes[i*4+3])<<24
 			}
 			output_channel <- samples
 		}
